cb8e290680206de8a07619e5004562eec0cbd3e6
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-07-16 20:59:56 +0300

    Part 01

--- Session 05/Program.cs
+++ Session 05/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //--------------------------------------------------- Part 01 ---------------------------------------------------
+            #region Looping | Iteration Statements
+
+            #region for - foreach
+
+            // //                0  1  2  3  4
+            int[] Numbers = { 1, 2, 3, 4, 5 };
+
+            //Console.WriteLine($"Number 0 is {Numbers[0]}");
+            //Console.WriteLine($"Number 1 is {Numbers[1]}");
+            //Console.WriteLine($"Number 2 is {Numbers[2]}");
+            //Console.WriteLine($"Number 3 is {Numbers[3]}");
+            //Console.WriteLine($"Number 4 is {Numbers[4]}");
+
+            //for (int i = 0; i < Numbers.Length; i++)
+            //{
+            //    //if (i == 2)
+            //        //continue;
+            //        //break;
+            //        //Numbers[i] += 10;
+            //        Console.WriteLine($"Number {i} is {Numbers[i]}");
+            //}
+
+            //foreach (int Item in Numbers)
+            //{
+            //    Console.WriteLine(Item + 10);
+            //}
+
+            #endregion
+
+            #endregion
         }
     }
 }
